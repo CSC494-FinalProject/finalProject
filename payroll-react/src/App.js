fe417b8eb3814de456a51fb70f6327a61a211fb8
Xander Chua <55899838+xander-cs@users.noreply.github.com>
2023-12-05 20:18:00 -0500

    real time payment template

--- payroll-react/src/App.js
+++ payroll-react/src/App.js
@@ -4,10 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>$ PAYR0LL</h1>
-      </header>
+  <header className="App-header">
+    <h1>$ PAYR0LL</h1>
+  </header>
+  
+  <div>
+    <h2>Real time payment:</h2>
+    <div>
+      <h4>Log in to employee account</h4>
+      <p>Username:</p>
+      <input/>
+      <p>Password:</p>
+      <input type="password"/>
     </div>
+    <div>
+      <h3>Salary earned so far:</h3>
+      <h2></b>$65.4321</b></h2>
+      <p>Clock in time: 07:28</p>
+      <p>Clock out time: ...</p>
+    </div>
+  </div>
+</div>
   );
 }
 
